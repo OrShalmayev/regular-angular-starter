c419de53d930e4c36767d6e438473f66d80bbe5f
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2022-06-24 19:06:42 +0300

    investigating persist state

--- src/app/shared/store/local-storage-state-storage.ts
+++ src/app/shared/store/local-storage-state-storage.ts
@@ -6,152 +6,152 @@ import { excludeKeys } from '../utils/exclude-keys';
 import { includeKeys } from '../utils/include-keys';
 
 import {
-  LocalStorageStateStorageConfig,
-  LocalStorageStateStorageConfigSpecialKey,
-  LocalStorageStateStorageConfigSpecialKeyType,
-  StorePersistConfigInternal,
-  StorePersistConfigSpecialKeysInternal,
+    LocalStorageStateStorageConfig,
+    LocalStorageStateStorageConfigSpecialKey,
+    LocalStorageStateStorageConfigSpecialKeyType,
+    StorePersistConfigInternal,
+    StorePersistConfigSpecialKeysInternal,
 } from './local-storage-state-storage-config';
 
 import { AnyObject } from '@shared/utils/type';
 
 interface TypeMap {
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  toStore: (value: any) => any;
-  toPersist: (value: any) => any;
-  /* eslint-enable @typescript-eslint/no-explicit-any */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    toStore: (value: any) => any;
+    toPersist: (value: any) => any;
+    /* eslint-enable @typescript-eslint/no-explicit-any */
 }
 
 const typeMap = new Map<LocalStorageStateStorageConfigSpecialKeyType | 'default', TypeMap>()
-  .set('set', {
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-    toStore: (value: any[] | undefined | null) => new Set(value ?? []),
-    toPersist: (value: Set<any> | undefined | null) => value && [...value],
-    /* eslint-enable @typescript-eslint/no-explicit-any */
-  })
-  .set('date', {
-    toStore: (value: string | null | undefined) => value && new Date(value),
-    toPersist: value => value,
-  })
-  .set('default', {
-    toStore: value => value,
-    toPersist: value => value,
-  });
+    .set('set', {
+        /* eslint-disable @typescript-eslint/no-explicit-any */
+        toStore: (value: any[] | undefined | null) => new Set(value ?? []),
+        toPersist: (value: Set<any> | undefined | null) => value && [...value],
+        /* eslint-enable @typescript-eslint/no-explicit-any */
+    })
+    .set('date', {
+        toStore: (value: string | null | undefined) => value && new Date(value),
+        toPersist: value => value,
+    })
+    .set('default', {
+        toStore: value => value,
+        toPersist: value => value,
+    });
 
 function filterSpecialKeys<T extends AnyObject>(
-  specialKey: Partial<StorePersistConfigSpecialKeysInternal<T>>
+    specialKey: Partial<StorePersistConfigSpecialKeysInternal<T>>
 ): specialKey is StorePersistConfigSpecialKeysInternal<T> {
-  return (
-    !specialKey.setToPersist && !!specialKey.setToStore && !!specialKey.getFromPersist && !!specialKey.getFromStore
-  );
+    return (
+        !specialKey.setToPersist && !!specialKey.setToStore && !!specialKey.getFromPersist && !!specialKey.getFromStore
+    );
 }
 
 export class LocalStorageStateStorage<T extends AnyObject> implements StateStorage {
-  private constructor(options: LocalStorageStateStorageConfig<T>) {
-    this._options = {
-      ...options,
-      specialKeys: this._parseSpecialKeys(options.specialKeys),
-    };
-  }
+    private constructor(options: LocalStorageStateStorageConfig<T>) {
+        this._options = {
+            ...options,
+            specialKeys: this._parseSpecialKeys(options.specialKeys),
+        };
+    }
 
-  protected readonly _options: StorePersistConfigInternal<T>;
+    protected readonly _options: StorePersistConfigInternal<T>;
 
-  private _parseSpecialKeys(
-    specialKeys: Array<LocalStorageStateStorageConfigSpecialKey<T>> | undefined
-  ): Array<StorePersistConfigSpecialKeysInternal<T>> | undefined {
-    let newSpecialKeys = specialKeys?.map((specialKey, index) => {
-      /* eslint-disable @typescript-eslint/no-explicit-any */
-      let getFromStore: ((state: T) => any) | undefined;
-      let setToStore: ((state: T, value: any) => T) | undefined;
-      let getFromPersist: ((state: T) => any) | undefined;
-      let setToPersist: ((state: T, value: any) => T) | undefined;
-      /* eslint-enable @typescript-eslint/no-explicit-any */
-      const typeFunctions = typeMap.get(specialKey.type ?? 'default')!;
-      if (specialKey.key) {
-        getFromStore = state => state[specialKey.key!];
-        setToStore = (state, value) => ({ ...state, [specialKey.key!]: value });
-      }
-      if (specialKey.get) {
-        getFromStore = specialKey.get;
-      }
-      if (specialKey.set) {
-        setToStore = specialKey.set;
-      }
-      if (getFromStore) {
-        getFromPersist = state => typeFunctions.toStore(getFromStore!(state));
-      }
-      if (setToStore) {
-        setToPersist = (state, value) => setToStore!(state, typeFunctions.toPersist(value));
-      }
-      if (!getFromStore || !getFromPersist) {
-        if (ngDevMode) {
-          throw new Error(
-            `Could not set the getters functions for Store at specialKeys[${index}]. "get" or "key" is required`
-          );
+    private _parseSpecialKeys(
+        specialKeys: Array<LocalStorageStateStorageConfigSpecialKey<T>> | undefined
+    ): Array<StorePersistConfigSpecialKeysInternal<T>> | undefined {
+        let newSpecialKeys = specialKeys?.map((specialKey, index) => {
+            /* eslint-disable @typescript-eslint/no-explicit-any */
+            let getFromStore: ((state: T) => any) | undefined;
+            let setToStore: ((state: T, value: any) => T) | undefined;
+            let getFromPersist: ((state: T) => any) | undefined;
+            let setToPersist: ((state: T, value: any) => T) | undefined;
+            /* eslint-enable @typescript-eslint/no-explicit-any */
+            const typeFunctions = typeMap.get(specialKey.type ?? 'default')!;
+            if (specialKey.key) {
+                getFromStore = state => state[specialKey.key!];
+                setToStore = (state, value) => ({ ...state, [specialKey.key!]: value });
+            }
+            if (specialKey.get) {
+                getFromStore = specialKey.get;
+            }
+            if (specialKey.set) {
+                setToStore = specialKey.set;
+            }
+            if (getFromStore) {
+                getFromPersist = state => typeFunctions.toStore(getFromStore!(state));
+            }
+            if (setToStore) {
+                setToPersist = (state, value) => setToStore!(state, typeFunctions.toPersist(value));
+            }
+            if (!getFromStore || !getFromPersist) {
+                if (ngDevMode) {
+                    throw new Error(
+                        `Could not set the getters functions for Store at specialKeys[${index}]. "get" or "key" is required`
+                    );
+                }
+            }
+            if (!setToStore || !setToPersist) {
+                if (ngDevMode) {
+                    throw new Error(
+                        `Could not set the setter functions for Store at specialKeys[${index}]. "set" of "key" is required`
+                    );
+                }
+            }
+            return {
+                ...specialKey,
+                getFromPersist,
+                getFromStore,
+                setToPersist,
+                setToStore,
+            };
+        });
+        if (!ngDevMode) {
+            newSpecialKeys = newSpecialKeys?.filter(filterSpecialKeys);
         }
-      }
-      if (!setToStore || !setToPersist) {
-        if (ngDevMode) {
-          throw new Error(
-            `Could not set the setter functions for Store at specialKeys[${index}]. "set" of "key" is required`
-          );
+        return newSpecialKeys as Array<StorePersistConfigSpecialKeysInternal<T>>;
+    }
+
+    async getItem<U extends AnyObject>(key: string): Promise<U | undefined> {
+        const item = localStorage.getItem(key);
+        if (!item) {
+            return undefined;
         }
-      }
-      return {
-        ...specialKey,
-        getFromPersist,
-        getFromStore,
-        setToPersist,
-        setToStore,
-      };
-    });
-    if (!ngDevMode) {
-      newSpecialKeys = newSpecialKeys?.filter(filterSpecialKeys);
+        let object = JSON.parse(item);
+        for (const { getFromPersist, setToStore } of this._options.specialKeys ?? []) {
+            object = setToStore(object, getFromPersist(object));
+        }
+        return object;
     }
-    return newSpecialKeys as Array<StorePersistConfigSpecialKeysInternal<T>>;
-  }
 
-  async getItem<U extends AnyObject>(key: string): Promise<U | undefined> {
-    const item = localStorage.getItem(key);
-    if (!item) {
-      return undefined;
+    async setItem(key: string, value: T): Promise<boolean> {
+        for (const { getFromStore, setToPersist } of this._options.specialKeys ?? []) {
+            value = setToPersist(value, getFromStore(value));
+        }
+        let object: Record<string, unknown> = value;
+        for (const ignoreKey of this._options.ignoreKeys ?? []) {
+            object = { ...value, [ignoreKey]: undefined };
+        }
+        localStorage.setItem(key, JSON.stringify(object));
+        return true;
     }
-    let object = JSON.parse(item);
-    for (const { getFromPersist, setToStore } of this._options.specialKeys ?? []) {
-      object = setToStore(object, getFromPersist(object));
-    }
-    return object;
-  }
 
-  async setItem(key: string, value: T): Promise<boolean> {
-    for (const { getFromStore, setToPersist } of this._options.specialKeys ?? []) {
-      value = setToPersist(value, getFromStore(value));
+    async removeItem(key: string): Promise<boolean> {
+        localStorage.removeItem(key);
+        return true;
     }
-    let object: Record<string, unknown> = value;
-    for (const ignoreKey of this._options.ignoreKeys ?? []) {
-      object = { ...value, [ignoreKey]: undefined };
+
+    static persistStore<S extends Store, State extends StoreValue<S>>(
+        store: S,
+        options: LocalStorageStateStorageConfig<State> = {}
+    ): ReturnType<typeof persistState> {
+        return persistState(store, {
+            storage: new LocalStorageStateStorage(options),
+            source: () =>
+                store.pipe(
+                    auditTime(500),
+                    includeKeys(options.includeKeys ?? []),
+                    excludeKeys(options.ignoreKeys ?? [])
+                ) as Observable<Partial<State>>,
+        });
     }
-    localStorage.setItem(key, JSON.stringify(object));
-    return true;
-  }
-
-  async removeItem(key: string): Promise<boolean> {
-    localStorage.removeItem(key);
-    return true;
-  }
-
-  static persistStore<S extends Store, State extends StoreValue<S>>(
-    store: S,
-    options: LocalStorageStateStorageConfig<State> = {}
-  ): ReturnType<typeof persistState> {
-    return persistState(store, {
-      storage: new LocalStorageStateStorage(options),
-      source: () =>
-        store.pipe(
-          auditTime(500),
-          includeKeys(options.includeKeys ?? []),
-          excludeKeys(options.ignoreKeys ?? [])
-        ) as Observable<Partial<State>>,
-    });
-  }
 }
