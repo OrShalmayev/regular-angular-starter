bab6b11d82ece4b0dd2cf43bb5788b38ae2c2e93
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2022-06-24 14:23:18 +0300

    refactored typing of record

--- src/app/core/services/window.service.ts
+++ src/app/core/services/window.service.ts
@@ -1,32 +1,30 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Injectable, InjectionToken, PLATFORM_ID } from '@angular/core';
+import { AnyObject } from '@shared/utils/type';
 
 export const WINDOW = new InjectionToken('WindowToken');
 
 export abstract class WindowRef {
-  getNativeWindow(): Window | Record<string, unknown> {
-    throw new Error('Not implemented.');
-  }
+    getNativeWindow(): Window | AnyObject {
+        throw new Error('Not implemented.');
+    }
 }
 
 @Injectable()
 export class BrowserWindowRef extends WindowRef {
-  override getNativeWindow(): Window | Record<string, unknown> {
-    return typeof window !== 'undefined' ? window : {};
-  }
+    override getNativeWindow(): Window | AnyObject {
+        return typeof window !== 'undefined' ? window : {};
+    }
 }
 
-export function windowFactory(
-  browserWindowRef: BrowserWindowRef,
-  platformId: string
-): Window | Record<string, unknown> {
-  if (isPlatformBrowser(platformId)) {
-    return browserWindowRef.getNativeWindow();
-  }
-  return {};
+export function windowFactory(browserWindowRef: BrowserWindowRef, platformId: string): Window | AnyObject {
+    if (isPlatformBrowser(platformId)) {
+        return browserWindowRef.getNativeWindow();
+    }
+    return {};
 }
 
 export const WINDOW_PROVIDERS = [
-  { provide: WindowRef, useClass: BrowserWindowRef },
-  { provide: WINDOW, useFactory: windowFactory, deps: [WindowRef, PLATFORM_ID] },
+    { provide: WindowRef, useClass: BrowserWindowRef },
+    { provide: WINDOW, useFactory: windowFactory, deps: [WindowRef, PLATFORM_ID] },
 ];
