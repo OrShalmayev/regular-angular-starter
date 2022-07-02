10127b64b97523c4a763869fd6a2fadf2b32a2f2
orshalmayev <orsh@ofakim-group.com>
2022-07-02 21:20:54 +0300

    changed error-field cmp to be more readable

--- src/app/shared/components/field-error/field-error.component.ts
+++ src/app/shared/components/field-error/field-error.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, AbstractControlDirective } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { isEmpty } from 'lodash';
 @Component({
     selector: 'shared-field-error',
     templateUrl: './field-error.component.html',
@@ -11,17 +11,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     imports: [CommonModule, MatFormFieldModule],
 })
 export class FieldErrorComponent {
-    constructor() {}
-
+    private readonly _defaultKey = 'default';
     errorMsgList: any = [];
-    private _default: 'default' = 'default';
-
-    @Input('controlName')
-    controlName!: AbstractControl | AbstractControlDirective;
+    @Input('controlName') controlName!: AbstractControl | AbstractControlDirective;
     @Input('HTMLType') HTMLType: 'REGULAR' | 'MATERIAL' = 'REGULAR';
 
     errorMessage: any = {
-        [this._default]: (params: any) => `Invalid format`,
+        [this._defaultKey]: (params: any) => `Invalid format`,
         required: (params: any) => `Field is required`,
         maxlength: (params: any) => `Maximum length of ${params.requiredLength} is required.`,
         minlength: (params: any) => `Minimum length of ${params.requiredLength} is required`,
@@ -30,24 +26,26 @@ export class FieldErrorComponent {
     };
 
     listErrors() {
-        if (!this.controlName) return [];
-        if (this.controlName.errors) {
-            this.errorMsgList = [];
-            Object.keys(this.controlName.errors).map(error => {
-                if (!Object.keys(this.errorMessage).includes(error)) {
-                    console.warn(
-                        `${error} didnt found at error component errorMessage object.\n setting error to default.`
-                    );
-                    error = this._default;
-                }
-                this.controlName.touched || this.controlName.dirty
-                    ? //@ts-ignore
-                      this.errorMsgList.push(this.errorMessage[error](this.controlName.errors[error]))
-                    : '';
-            });
-            return this.errorMsgList;
-        } else {
-            return [];
-        }
+        if (!this.controlName || isEmpty(this.controlName.errors)) return [];
+
+        this.errorMsgList = [];
+
+        const controlErrKeyArr = Object.keys(<any>this.controlName.errors);
+
+        controlErrKeyArr.map(error => {
+            const errorMsgIncludesError: boolean = Object.keys(this.errorMessage).includes(error);
+
+            if (!errorMsgIncludesError) {
+                console.warn(
+                    `${error} didnt found at error component errorMessage object.\n setting error to default.`
+                );
+                error = this._defaultKey;
+            }
+            this.controlName.touched || this.controlName.dirty
+                ? //@ts-ignore
+                  this.errorMsgList.push(this.errorMessage[error](this.controlName.errors[error]))
+                : '';
+        });
+        return this.errorMsgList;
     }
 }
