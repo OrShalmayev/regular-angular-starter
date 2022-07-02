11e1aacc471b1c99a459e544fcdd3ccfb5f51c7c
orshalmayev <orsh@ofakim-group.com>
2022-07-02 21:36:36 +0300

    refactored app cmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,13 @@
-import { registerLocaleData, TitleCasePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import localeEN from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
 import { MatDateFnsModule } from '@angular/material-date-fns-adapter';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import dateFnsLocaleEN from 'date-fns/locale/en-US';
-import { CURRENCY_MASK_CONFIG, CurrencyMaskConfig } from 'ngx-currency';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LocaleMonthsProvider } from './core/tokens/locale-months.token';
 
-import { GlobalConstantsProvider } from './settings';
 import { CoreModule } from '@core/core.module';
-
-registerLocaleData(localeEN);
-
-function currencyMaskConfigFactory(): CurrencyMaskConfig {
-    return {
-        align: 'left',
-        decimal: ',',
-        allowZero: true,
-        nullable: false,
-        precision: 2,
-        thousands: '.',
-        prefix: '',
-        suffix: '',
-        allowNegative: true,
-    };
-}
-
 @NgModule({
     declarations: [AppComponent],
     imports: [
@@ -44,14 +18,7 @@ function currencyMaskConfigFactory(): CurrencyMaskConfig {
         CoreModule,
         MatDateFnsModule,
     ],
-    providers: [
-        { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } as MatFormFieldDefaultOptions },
-        { provide: MAT_DATE_LOCALE, useValue: dateFnsLocaleEN },
-        { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 5000 } as MatSnackBarConfig },
-        { provide: CURRENCY_MASK_CONFIG, useFactory: currencyMaskConfigFactory },
-        LocaleMonthsProvider,
-        GlobalConstantsProvider,
-    ],
+    providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
