213c61ac234dd6750ce6bb12086f90d543817f03
orshalmayev <orsh@ofakim-group.com>
2022-07-23 18:31:38 +0300

    removed unused stuff

--- src/app/core/utility/rxjs/convertObservableToBehaviorSubject.ts
+++
@@ -1,7 +0,0 @@
-import { BehaviorSubject, Observable } from 'rxjs';
-
-export function convertObservableToBehaviorSubject<T = any>(source: Observable<T>): BehaviorSubject<T> {
-  const mappedSubject = new BehaviorSubject<T>([] as any);
-  source.subscribe(value => mappedSubject.next(value));
-  return mappedSubject;
-}
