61ae58c3cd6f528fffabe0f76b7185afbbce5301
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2022-12-15 09:39:37 +0200

    Update operators.ts

--- src/app/shared/utils/rxjs/operators.ts
+++ src/app/shared/utils/rxjs/operators.ts
@@ -41,7 +41,7 @@ export const debug = function debug(tag: string) {
       console.log(`%c[${tag}: Next]`, "background: #009688; color: #fff; padding: 3px; font-size: 9px;", value)
     },
     error(error) {
-      console.log(`%[${tag}: Error]`, "background: #E91E63; color: #fff; padding: 3px; font-size: 9px;", error)
+      console.log(`%c[${tag}: Error]`, "background: #E91E63; color: #fff; padding: 3px; font-size: 9px;", error)
     },
     complete() {
       console.log(`%c[${tag}]: Complete`, "background: #00BCD4; color: #fff; padding: 3px; font-size: 9px;")
