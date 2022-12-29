e8761bebd095871d908eb55b2772aa83f62c9135
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2022-12-29 17:34:40 +0200

    ADDED TYPE TO DEBUG

--- src/app/shared/utils/rxjs/operators.ts
+++ src/app/shared/utils/rxjs/operators.ts
@@ -35,16 +35,16 @@ export const reduceTo = <T, K extends ConditionalKeys<T, any[]>>(key: K): Operat
     return reduceToFunc(array, key);
   });
 
-export const debug = function debug(tag: string) {
-  return tap({
-    next(value) {
-      console.log(`%c[${tag}: Next]`, "background: #009688; color: #fff; padding: 3px; font-size: 9px;", value)
-    },
-    error(error) {
-      console.log(`%c[${tag}: Error]`, "background: #E91E63; color: #fff; padding: 3px; font-size: 9px;", error)
-    },
-    complete() {
-      console.log(`%c[${tag}]: Complete`, "background: #00BCD4; color: #fff; padding: 3px; font-size: 9px;")
-    }
-  })
+export const debug = function debug<T>(tag: string):MonoTypeOperatorFunction<T> {
+    return tap({
+        next(value) {
+          console.log(`%c[${tag}: Next]`, "background: #009688; color: #fff; padding: 3px; font-size: 9px;", value)
+        },
+        error(error) {
+          console.log(`%c[${tag}: Error]`, "background: #E91E63; color: #fff; padding: 3px; font-size: 9px;", error)
+        },
+        complete() {
+          console.log(`%c[${tag}]: Complete`, "background: #00BCD4; color: #fff; padding: 3px; font-size: 9px;")
+        }
+    })
 }
